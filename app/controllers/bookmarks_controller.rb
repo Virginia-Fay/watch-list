69be2fc836200990f41dec115489903ac4f32bec
virginie lty <virginielty@gmail.com>
2022-01-04 20:36:07 +0100

    correction

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,20 +1,77 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
+
   def new
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    @list = List.find(params[:list_id])
     @bookmark.list = @list
-    @bookmark.save
-    redirect_to bookmark_path(@bookmark)
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
   end
 
   private
 
   def bookmark_params
-    params.require(:review).permit(:comment, :movie_id)
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
+
+# class BookmarksController < ApplicationController
+#   def new
+#     @list = List.find(params[:list_id])
+#     @bookmark = Bookmark.new
+#   end
+
+#   def create
+#     @bookmark = Bookmark.new(bookmark_params)
+#     @list = List.find(params[:list_id])
+#     @bookmark.list = @list
+#     if @bookmark.save
+#       redirect_to list_path(@list)
+#     else
+#       render :new
+#     end
+#     # @bookmark.save
+#     # redirect_to bookmark_path(@bookmark)
+#   end
+
+#   def destroy
+#     @bookmark.destroy
+#     redirect_to list_path(@bookmark.list)
+#   end
+
+#   private
+
+#   def bookmark_params
+#     params.require(:bookmark).permit(:comment, :movie_id)
+#   end
+
+
+#   def set_bookmark
+#     @bookmark = Bookmark.find(params[:id])
+#   end
+
+#   def set_list
+#     @list = List.find(params[:list_id])
+#   end
+# end
